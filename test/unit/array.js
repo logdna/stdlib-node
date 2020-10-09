02606696915eba829bd811145c9a35bee36ca346
Mike Del Tito <mike.deltito@logdna.com>
2020-10-09 12:28:21 -0400

    feat(array): allow custom separator in array.toArray
    Add support for a custom separator in array.toArray
    for less-common scenarios.

--- test/unit/array.js
+++ test/unit/array.js
@@ -12,12 +12,20 @@ test('array', async (t) => {
     , {value: '', expected: [], message: 'toArray(\'\') == []'}
     , {value: 'test', expected: ['test']}
     , {value: '1,2,3', expected: ['1', '2', '3']}
+    , {value: '1, 2, 3', expected: ['1', '2', '3']}
+    , {value: '1, 2, 3', expected: ['1', ' 2', ' 3'], sep: ','}
+    , {value: '1|2|3', expected: ['1', '2', '3'], sep: '|'}
     , {value: [1, 2, 3], expected: [1, 2, 3]}
     , {value: new Set([1, null, 'test']), expected: [1, null, 'test']}
     ]
     for (const current of cases) {
+      const args = [current.value]
+      if (current.sep) {
+        args.push(current.sep)
+      }
+
       t.deepEqual(
-        toArray(current.value)
+        toArray(...args)
       , current.expected
       , current.message || `toArray(${current.value}) == ${current.expected}`
       )
