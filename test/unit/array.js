f08b9ca3acca6e173ae3fca0b8279fb85d38229a
Mike Del Tito <mike.deltito@logdna.com>
2020-12-08 17:42:23 -0500

    feat(array): add array.chunk function
    This adds a `chunk` function for splitting arrays
    into smaller arrays with a defined `size`. It does not
    modify the input array.

--- test/unit/array.js
+++ test/unit/array.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const {test, threw} = require('tap')
-const toArray = require('../../lib/array/to-array.js')
+const array = require('../../lib/array/index.js')
 
 test('array', async (t) => {
   t.test('toArray', async (t) => {
@@ -25,10 +25,78 @@ test('array', async (t) => {
       }
 
       t.deepEqual(
-        toArray(...args)
+        array.toArray(...args)
       , current.expected
       , current.message || `toArray(${current.value}) == ${current.expected}`
       )
     }
+  })
+
+  test('chunk', async (t) => {
+    t.test('throws on invalid input array', async (t) => {
+      t.throws(() => { array.chunk('not an array', 2) }, {
+        name: 'TypeError'
+      , message: 'first argument must be an array'
+      , meta: {
+          arr: 'not an array'
+        , type: 'string'
+        }
+      })
+    })
+
+    t.test('throws on invalid size', async (t) => {
+      t.throws(() => { array.chunk([1, 2, 3], 'bad') }, {
+        name: 'TypeError'
+      , message: 'second argument must be an integer'
+      , meta: {
+          size: 'bad'
+        , type: 'string'
+        }
+      })
+
+      t.throws(() => { array.chunk([1, 2, 3], 1.2345) }, {
+        name: 'TypeError'
+      , message: 'second argument must be an integer'
+      , meta: {
+          size: 1.2345
+        , type: 'number'
+        }
+      })
+
+      t.throws(() => { array.chunk([1, 2, 3], 0) }, {
+        name: 'RangeError'
+      , message: 'size must be >= 1'
+      , meta: {
+          size: 0
+        }
+      })
+
+      t.throws(() => { array.chunk([1, 2, 3], -2) }, {
+        name: 'RangeError'
+      , message: 'size must be >= 1'
+      , meta: {
+          size: -2
+        }
+      })
+    })
+
+    t.test('chunks empty array', async (t) => {
+      const input = []
+      const result = array.chunk([], 2)
+      t.deepEqual(input, [], 'empty array')
+      t.notStrictEqual(input, result, 'new array created')
+    })
+
+    t.test('chunks input array', async (t) => {
+      t.deepEqual(array.chunk([1, 2, 3], 1), [[1], [2], [3]], 'single element chunks')
+      t.deepEqual(
+        array.chunk([1, 2, 3, 4, 5], 2)
+      , [[1, 2], [3, 4], [5]]
+      , 'remainder in separate chunk')
+      t.deepEqual(
+        array.chunk([{a: 'b'}, {c: 'd'}, {e: 'f'}], 2)
+      , [[{a: 'b'}, {c: 'd'}], [{e: 'f'}]]
+      , 'chunks an array of objects')
+    })
   })
 }).catch(threw)
